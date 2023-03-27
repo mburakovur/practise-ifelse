a05d67edd7a6ca86a80fe5f14ecd0bdfa23e8b22
M. Burak Ovur <mburak.ovur@gmail.com>
2023-03-27 13:13:44 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+int time = DateTime.Now.Hour;
+if(time>=6 && time<11)
+    Console.WriteLine("Good Morning maaam");
+else if (time<=18)
+    Console.WriteLine("Have a Good Day!");
+else 
+    Console.WriteLine("Good Night Guys");
+
+string sonuc = time>=6 && time<11 ? "GÜNAYDIN!" : "Yatma MK" ;
+Console.WriteLine("sonuç" + sonuc);
